761a5879125320abdc6c10db241082ed8599ab51
jersrej <jerson.conmigo@gmail.com>
2020-03-24 19:30:41 +0800

    fix | spaces
    fix | spaces

--- src/libs/helpers.js
+++ src/libs/helpers.js
@@ -3,29 +3,29 @@ import axios from 'axios';
 import constants from './constants';
 
 const helpers = {
-    fetchPokemon: function (apiMethod) {
-        try {
-            const pokemon = axios(`${constants.poke_api}${apiMethod}`);
+	fetchPokemon: function (apiMethod) {
+		try {
+			const pokemon = axios(`${constants.poke_api}${apiMethod}`);
 
-            return pokemon;
-        } catch (err) {
-            console.log(err);
-            throw err;
-        }
-    },
-    fetchMultiple: function (methodArr = []) {
-        try {
-            const pokemon = axios.all(methodArr)
+			return pokemon;
+		} catch (err) {
+			console.log(err);
+			throw err;
+		}
+	},
+	fetchMultiple: function (methodArr = []) {
+		try {
+			const pokemon = axios.all(methodArr)
 
-            return pokemon;
-        } catch (err) {
-            console.log(err);
-            throw err;
-        }
-    },
-    fetchSprite: function (pokeId) {
-        return <img src={constants.poke_api_sprites + pokeId + '.png'} alt="Pokemon" />;
-    }
+			return pokemon;
+		} catch (err) {
+			console.log(err);
+			throw err;
+		}
+	},
+	fetchSprite: function (pokeId) {
+		return <img src={constants.poke_api_sprites + pokeId + '.png'} alt="Pokemon" />;
+	}
 };
 
 export default helpers;
